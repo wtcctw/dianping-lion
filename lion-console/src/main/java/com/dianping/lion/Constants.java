ddd577481ef7f32787a31abd4a7529bd7823695e
xiangbin.miao <xiangbin.miao@dianping.com>
2012-07-10 22:41:15 +0800

    add constants and ftl

---
+++ lion-console/src/main/java/com/dianping/lion/Constants.java
@@ -0,0 +1,9 @@
+package com.dianping.lion;
+
+public class Constants {
+	
+	public static final String PROJECT_NAME = "project";
+	public static final String PRODUCT_NAME = "product";
+	public static final String TEAM_NAME = "team";
+
+}
